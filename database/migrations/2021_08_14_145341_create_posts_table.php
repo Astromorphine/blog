3308d8f6008833294f9336292d316179abff939b
Vitaliy Tikh <63010229+Astromorphine@users.noreply.github.com>
2021-08-14 22:26:30 +0300

    added npm/bootstrap

--- database/migrations/2021_08_14_145341_create_posts_table.php
+++ database/migrations/2021_08_14_145341_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('post_id');
+            $table->string('title', 100);
+            $table->string('excerpt', 200);
+            $table->string('image', 100)->nullable();
+            $table->text('body');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
